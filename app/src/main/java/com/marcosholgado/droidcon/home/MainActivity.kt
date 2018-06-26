497ab32639b200b8fc13c30d0148dd0d1ca1eb4e
Marcos Holgado <marcos.holgadomolina@sky.uk>
2018-06-26 10:30:51 +0100

    Create new module for articles and implement navigation

---
+++ app/src/main/java/com/marcosholgado/droidcon/home/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.marcosholgado.droidcon.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.marcosholgado.droidcon.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.mainNavFragment)
+        setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+    }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
+}
