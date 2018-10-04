00e789a16c934d5495a43f39826eae021d2a60f6
Marcos Holgado <marcos.holgadomolina@sky.uk>
2018-10-04 16:04:40 +0100

    Remove unnecessary code

--- app/src/main/java/com/marcosholgado/droidcon/home/features/top_stories/TopStoriesFragment.kt
+++ app/src/main/java/com/marcosholgado/droidcon/home/features/top_stories/TopStoriesFragment.kt
@@ -1,6 +1,6 @@
 package com.marcosholgado.droidcon.home.features.top_stories
 
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.marcosholgado.articlelist.ArticleListFragment
 import com.marcosholgado.droidcon.home.features.top_stories.TopStoriesFragmentDirections.action_articles_to_reader
 
@@ -8,8 +8,6 @@ class TopStoriesFragment: ArticleListFragment() {
 
     override fun onItemSelected(url: String) {
         val navDirections = action_articles_to_reader(url)
-        view?.let {
-            findNavController(it).navigate(navDirections)
-        }
+        findNavController().navigate(navDirections)
     }
 }
